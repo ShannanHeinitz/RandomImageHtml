baf84fe9ca6188e2b7836211321e7b8703e3f0db
ShannanHeinitz <ShannanHeinitz@users.noreply.github.com>
2021-02-07 14:44:54 +1100

    Add files via upload

---
+++ RandomImageList.js
@@ -0,0 +1,6 @@
+var RandomImageList=new Array() 
+RandomImageList[1]="file:///C:/Windows/Web/Wallpaper/Theme1/img1.jpg" 
+RandomImageList[2]="file:///C:/Windows/Web/Wallpaper/Theme1/img13.jpg" 
+RandomImageList[3]="file:///C:/Windows/Web/Wallpaper/Theme1/img2.jpg" 
+RandomImageList[4]="file:///C:/Windows/Web/Wallpaper/Theme1/img3.jpg" 
+RandomImageList[5]="file:///C:/Windows/Web/Wallpaper/Theme1/img4.jpg" 
